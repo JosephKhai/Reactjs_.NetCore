3afca4f6db834f3d0fbd2b55c62bdea7dcbde033
Pum Suan Khai <josephkhaipi@gmail.com>
2022-02-09 23:00:54 +1100

    Add note

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,7 +43,6 @@ namespace API
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
 
-          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
